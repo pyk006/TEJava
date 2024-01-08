a8645c2e0cdb3432dba884b3e18a6684f6882aab
Phil Kim <43870539+pyk006@users.noreply.github.com>
2024-01-08 15:07:42 -0800

    Update create.sh

--- java/database/create.sh
+++ java/database/create.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PGPASSWORD='XWJ91dyfppBCyMEZEYxg'
+export PGPASSWORD='2c16FG6E4fGfC4E6AcGcaD6CAfb2FFCD'
 BASEDIR=$(dirname $0)
 for i in 18 17 16 15 14 13 12 11 10 9
 do
@@ -8,10 +8,10 @@ do
     if [ -d "$file" ]
     then
         echo "Postgres version $i found."
-        "$FILEPATH/psql.exe" -h containers-us-west-135.railway.app -U postgres -p 7411 -d railway -f "$BASEDIR/dropdb.sql" &&
-        "$FILEPATH/psql.exe" -h containers-us-west-135.railway.app -U postgres -p 7411 -d railway -f "$BASEDIR/schema.sql" &&
-        "$FILEPATH/psql.exe" -h containers-us-west-135.railway.app -U postgres -p 7411 -d railway -f "$BASEDIR/data.sql" &&
-        "$FILEPATH/psql.exe" -h containers-us-west-135.railway.app -U postgres -p 7411 -d railway -f "$BASEDIR/user.sql"
+        "$FILEPATH/psql.exe" -h roundhouse.proxy.rlwy.net -U postgres -p 55996 -d railway -f "$BASEDIR/dropdb.sql" &&
+        "$FILEPATH/psql.exe" -h roundhouse.proxy.rlwy.net -U postgres -p 55996 -d railway -f "$BASEDIR/schema.sql" &&
+        "$FILEPATH/psql.exe" -h roundhouse.proxy.rlwy.net -U postgres -p 55996 -d railway -f "$BASEDIR/data.sql" &&
+        "$FILEPATH/psql.exe" -h roundhouse.proxy.rlwy.net -U postgres -p 55996 -d railway -f "$BASEDIR/user.sql"
         break
     fi
 done
