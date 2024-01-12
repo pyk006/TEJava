31e6d6563cef80b0028effde0cfd69763c7b9418
Phil Kim <43870539+pyk006@users.noreply.github.com>
2024-01-12 12:48:34 -0800

    Update AdopterController.java

--- java/src/main/java/com/techelevator/controller/AdopterController.java
+++ java/src/main/java/com/techelevator/controller/AdopterController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "https://pyk006.github.io")
+@CrossOrigin(origins = "https://pyk006.github.io/TEVue/")
 public class AdopterController {
 
     @Autowired
